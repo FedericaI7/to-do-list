c5bce193e69e4e674ab7d958b2d8759d524a5648
FedericaI7 <federicaiuvara3@gmail.com>
2023-09-06 16:05:01 +0200

    Add: create two state and two functions

--- src/App.js
+++ src/App.js
@@ -3,14 +3,39 @@ import "./App.css";
 import Header from "./Header";
 
 function App() {
+  // State for input value and array of items
   const [inputValue, setInputValue] = useState("");
+  const [arr, setArr] = useState([]);
+
+  // Function to handle input change
+  const changeInput = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  // Function to add a new item to the array
+  const clickButton = () => {
+    // Create a new object with an incremental ID (necessary for delete a specific element on the list)
+    let newObj = {
+      id: arr.length + 1,
+      name: inputValue,
+    };
+    // Add the new object to the array
+    setArr([...arr, setArr]);
+    // Reset the input
+    setInputValue("");
+  };
 
   return (
     <>
       <Header />
       <div className="app--container">
-        <input value={inputValue} type="text" placeholder="" />
-        <button>+</button>
+        <input
+          value={inputValue}
+          onChange={changeInput}
+          type="text"
+          placeholder=""
+        />
+        <button onClick={clickButton}>+</button>
       </div>
     </>
   );
