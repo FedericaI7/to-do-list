f130797ee1158c21b7d83f7d139da10894113a5e
FedericaI7 <federicaiuvara3@gmail.com>
2023-09-06 15:11:16 +0200

    Header: create component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="app--container">
+        <input value={inputValue} type="text" placeholder="" />
+        <button>Add</button>
+      </div>
+    </>
   );
 }
 
